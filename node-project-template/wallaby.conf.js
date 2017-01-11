3196b88c1f81dc255482ef934cec23d7b8df1ed9
pietvanzoen <pietvanzoen@gmail.com>
2017-01-11 08:12:46 +0100

    Add jasmine setup file and update circle runner

--- node-project-template/wallaby.conf.js
+++ node-project-template/wallaby.conf.js
@@ -3,6 +3,9 @@ module.exports = function() {
     files: ['lib/**/*.js', 'test/_helpers/**/*'],
     tests: ['test/**/*.spec.js'],
     testFramework: 'jasmine',
+    setup: function() {
+      require('./test/_helpers/setup')
+    },
     env: {
       type: 'node',
       // runner: 'path/to/your/node/executable'
