685484cb1ee06e56b26c6dce9572eab56144bd53
Gijs Kunze <gwkunze@gmail.com>
2017-04-10 17:30:43 +0200

    Some improvements (imho) to node template (#26)
    * Some improvements (imho) to node template
    
    - Removed FEDS from PR template
    - Added jamine elsint plugin (no more need for lint-tests.sh)
    - Moved dependencies to devDependencies
    - Removed lodash (wasn't used)
    
    * Recommented out eslint extend
    
    * Ignore node dir
    
    * linebreak

--- node-project-template/.github/PULL_REQUEST_TEMPLATE.md
+++ node-project-template/.github/PULL_REQUEST_TEMPLATE.md
@@ -1,4 +1,4 @@
-Closes FED-.
+Closes 
 
 Reviewers: @
 
@@ -6,4 +6,4 @@ Reviewers: @
 - 
 
 ## Changelog:
--
+- 
--- node-project-template/lint-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-focused_tests="$(grep -r -n 'fdescribe(\|fit(' ./test)"
-
-[[ -z $focused_tests ]] && exit 0;
-
-echo "*** Focused tests found. Please fix: ***"
-echo "$focused_tests"
-exit 1
